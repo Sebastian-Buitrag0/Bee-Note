82f57ff2f41b631e2b3f3eea73d9186dc6e6bbc9
Juan-S-L <jsebastianlu16@gmail.com>
2024-04-27 14:18:21 -0500

    Se implementó Picker de galleria y foto para que el usuario pudiera subir una imagen al proyecto como portada

--- front_end/bee_note_application/lib/widgets/formulario_proyecto.dart
+++ front_end/bee_note_application/lib/widgets/formulario_proyecto.dart
@@ -1,5 +1,10 @@
-import 'package:bee_note_application/widgets/widgsts.dart';
 import 'package:flutter/material.dart';
+
+import 'package:bee_note_application/widgets/widgsts.dart';
+import 'dart:io';
+import 'dart:typed_data';
+
+import 'package:image_picker/image_picker.dart';
 
 class FormProyect extends StatefulWidget {
   const FormProyect({super.key});
@@ -12,6 +17,9 @@ class _FormProyetcStaet extends State<FormProyect> {
 
   final projectNamwController = TextEditingController();
   final descriptionController = TextEditingController();
+
+  Uint8List? _image;
+  File? selectedImage;
 
   @override
   Widget build(BuildContext context) {
@@ -33,15 +41,190 @@ class _FormProyetcStaet extends State<FormProyect> {
           ),
 
           _ParticipantButton(
-            onTap: () {
-              print('Funciona');
-            },
-          )
+            onTap: () => Navigator.pushNamed(context, 'collaborator'),
+          ),
 
+          _BuildSelectedImage(
+            onTapContainer: () => showImagePckerOption(context),
+            onTapText: () => showImagePckerOption(context),
+            image: _image,
+          ),
+
+          const SizedBox(height: 20,),
+          
+          HexagonalButton(
+            onTap:() {
+              // todo: funcion para guardar los campos de nuevo proyecto
+            },
+            text: 'Guardar',
+          ),
+
+          const SizedBox(height: 20,),
         ],
       ),
     );
   }
+
+  // Mostrar imagen en Avatar
+  void showImagePckerOption(BuildContext context){
+    showModalBottomSheet(
+      backgroundColor: Colors.amber[200],
+      context: context, 
+      builder: (builder){
+
+        return Padding(
+          padding: const EdgeInsets.all(28.0),
+          child: SizedBox(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height / 6.5,
+            child: Row(
+              children: [
+                
+                Expanded(
+                  child: InkWell(
+                    onTap: (){
+                      _pickerImageFromGallery();
+                    },
+                    child: const SizedBox(
+                      child: Column(
+                        children: [
+                          Icon(Icons.image, size: 70,color: Color(0xFF3b486a),),
+                          Text('Galeria')
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+          
+                Expanded(
+                  child: InkWell(
+                    onTap: (){
+                      _pickerImageFromCamera();
+                    },
+                    child: const SizedBox(
+                      child: Column(
+                        children: [
+                          Icon(Icons.camera_alt, size: 70,color: Color(0xFF3b486a),),
+                          Text('Camara')
+                        ],
+                      ),
+                    ),
+                  ),
+                )
+
+              ],
+            ),
+          ),
+        );
+      }
+    );
+  }
+
+  // Cargar imagen de galeria
+  Future _pickerImageFromGallery() async{
+    final returnImage = await ImagePicker().pickImage(source: ImageSource.gallery);
+
+    if(returnImage == null) return;
+
+    setState(() {
+      selectedImage = File(returnImage.path);
+      _image = File(returnImage.path).readAsBytesSync();
+    });
+    Navigator.of(context).pop();
+  }
+
+  // Cargar imagen de foto
+  Future _pickerImageFromCamera() async{
+    final returnImage = await ImagePicker().pickImage(source: ImageSource.camera);
+
+    if(returnImage == null) return;
+
+    setState(() {
+      selectedImage = File(returnImage.path);
+      _image = File(returnImage.path).readAsBytesSync();
+    });
+    Navigator.of(context).pop();
+  }
+}
+
+class _BuildSelectedImage extends StatelessWidget {
+
+  final Function() onTapContainer;
+  final Function() onTapText;
+  final Uint8List? image;
+
+  const _BuildSelectedImage({
+    required this.onTapContainer, 
+    required this.onTapText, 
+    this.image ,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 7.5),
+
+      child: GestureDetector(
+
+        onTap: onTapContainer,
+
+        child: Container(
+        
+          width: double.infinity,
+          decoration: _buildBoxDecoration(),
+        
+          height: 200,
+
+          child: image != null
+            ? ClipRRect(
+              borderRadius: BorderRadius.circular(30),
+              child: SizedBox(
+                width: double.infinity,
+                height: 200,
+                child: Image.memory(
+                  image!,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            )
+            : Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Icon(Icons.upload_file, size: 100,),
+                GestureDetector(
+                  onTap: onTapText,
+                  child: const Text(
+                    'Subir foto',
+                    style: TextStyle(
+                      fontFamily: 'Letters_for_Learners',
+                      fontSize: 30,
+                      color: Color(0xFF3b486a),
+                      decoration: TextDecoration.underline,
+                      decorationColor: Color(0xFF3b486a)
+                    ),
+                  ),
+                ),
+              ],
+            ),
+
+        ),
+      ),
+    );
+  }
+  BoxDecoration _buildBoxDecoration() {
+    return BoxDecoration(
+      borderRadius: BorderRadius.circular(30),
+      border: Border.all(color: Colors.white),
+      color: Colors.white,
+      boxShadow: const [
+        BoxShadow(
+          color: Colors.black12,
+          offset: Offset(0, 15),
+          blurRadius: 10,
+        ),
+      ],
+    );
+  }  
 }
 
 class _ParticipantButton extends StatelessWidget {
@@ -111,18 +294,7 @@ class _BuilTextDescription extends StatelessWidget {
         width: double.infinity,
         padding: const EdgeInsets.symmetric(horizontal: 25),
         
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(30),
-          border: Border.all(color: Colors.white),
-          color: Colors.white,
-          boxShadow: const [
-            BoxShadow(
-              color: Colors.black12,
-              offset: Offset(0, 15),
-              blurRadius: 10,
-            ),
-          ],
-        ),
+        decoration: _buildBoxDecoration(),
       
         child: TextFormField(
 
@@ -142,6 +314,21 @@ class _BuilTextDescription extends StatelessWidget {
           ),
         )
       ),
+    );
+  }
+
+  BoxDecoration _buildBoxDecoration() {
+    return BoxDecoration(
+      borderRadius: BorderRadius.circular(30),
+      border: Border.all(color: Colors.white),
+      color: Colors.white,
+      boxShadow: const [
+        BoxShadow(
+          color: Colors.black12,
+          offset: Offset(0, 15),
+          blurRadius: 10,
+        ),
+      ],
     );
   }
 }
