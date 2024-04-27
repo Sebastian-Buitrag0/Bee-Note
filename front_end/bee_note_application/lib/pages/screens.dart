abd1938863ed2d4e04fd9ac75cfb7445e7528bd9
Juan-S-L <jsebastianlu16@gmail.com>
2024-04-27 14:11:12 -0500

    Se elimina elemento innecesario

--- front_end/bee_note_application/lib/pages/screens.dart
+++
@@ -1,5 +0,0 @@
-export 'package:bee_note_application/pages/project_page.dart';
-export 'package:bee_note_application/pages/home_page.dart';
-export 'package:bee_note_application/pages/login_page.dart';
-export 'package:bee_note_application/pages/register_page_pt1.dart';
-export 'package:bee_note_application/pages/register_page_pt2.dart';
