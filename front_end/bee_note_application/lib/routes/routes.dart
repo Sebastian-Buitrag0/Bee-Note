4c6121a1886d9d5f731bbe6e1ee40b227aaebcb8
Juan-S-L <jsebastianlu16@gmail.com>
2024-05-07 10:27:06 -0500

    se elimino la ruta de task y se modifico la ruta de proyecto

--- front_end/bee_note_application/lib/routes/routes.dart
+++ front_end/bee_note_application/lib/routes/routes.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 
 import 'package:bee_note_application/pages/collaborator_page.dart';
-import 'package:bee_note_application/pages/task_page.dart';
+import 'package:bee_note_application/pages/create_task_page.dart';
+// import 'package:bee_note_application/pages/task_page.dart';
 import 'package:bee_note_application/pages/home_page.dart';
 import 'package:bee_note_application/pages/login_page.dart';
-import 'package:bee_note_application/pages/project_page.dart';
+import 'package:bee_note_application/pages/create_project_page.dart';
 import 'package:bee_note_application/pages/register_page_pt1.dart';
 import 'package:bee_note_application/pages/register_page_pt2.dart';
 
@@ -13,7 +14,8 @@ final Map<String, Widget Function(BuildContext)> appRoutes = {
   'register1': ( _ ) => const RegisterPage1(),
   'register2': ( _ ) => const RegisterPage2(),
   'home': ( _ ) => const HomePage(),
-  'project': ( _ ) => const ProjectScreen(),
-  'task': ( _ ) => const TaskPage(),
+  'create_project': ( _ ) => const CreateProjectScreen(),
+  // 'task': ( _ ) => const TaskPage(),
   'collaborator': ( _ ) => const CollaboratorPage(),
+  'create_task': ( _ ) => const CreateTaskScreen(),
 };
